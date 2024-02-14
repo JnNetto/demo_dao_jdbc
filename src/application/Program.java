e13cb97fceb0f51e7f794d8b40a2403c2032e3e9
João Neto <jagomes694@gmail.com>
2024-02-14 15:11:06 -0300

    SellerDaoJDBC e DaoFactory criados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
@@ -9,6 +11,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Seller dep = new Seller(1, "Book", "fds", new Date(), 12.0);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(dep);
 	}
 
